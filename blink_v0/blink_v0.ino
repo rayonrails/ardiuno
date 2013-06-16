a67d8f09d206921a932228013eb570c87db1911e
Ray Rogers <ray@rayonrails.com>
2013-06-15 22:35:40 -0400

    lesson CIRC-01 - blinking light

---
+++ blink_v0/blink_v0.ino
@@ -0,0 +1,19 @@
+/* Blink
+*  Turns on an LED on for one second, then off for one second,
+*  repeatedly.
+*  Credated 15 June 2013
+*/
+int ledPin  = 13; // LED connected to digital pin 13
+
+// the setup() metho runs ones, when the sketch starts
+void setup() {
+  pinMode(ledPin, OUTPUT); }
+  
+// the loop() method runs over and over again.
+// as long as the Arduino has power
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
